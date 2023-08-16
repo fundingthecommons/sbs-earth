b12c96fe4f32e485885b4a2f1d6ce5c8b8523f38
Eric Ronne <eric@protocol.ai>
2023-08-16 14:07:15 -0500

    Video player content updates for post-event

--- components/blocks/video-player.tsx
+++ components/blocks/video-player.tsx
@@ -126,9 +126,9 @@ export const VideoPlayer = ({ data }) => {
     >
       <div className={`relative max-w-site-full mx-auto py-10 px-10 sm:px-0`}>
         <h2 className="text-accent3 mg-headline-medium text-center mt-12 mb-16 sm:mb-2">
-          Explore Projects
+          Explore SBS Earth Content
         </h2>
-        <p className="text-white text-center mg-copy">As part of this event, VOD (video on demand) talks will be featured on the interactive map below, allowing participants to enjoy presentations at their convenience. Be sure to check out this first of its kind feature during this groundbreaking digital experience!</p>
+        <p className="text-white text-center mg-copy">SBS Earth featured talks from industry leaders, innovators, and sustainability enthusiasts from all around the globe. Be sure to check out the replays on the interactive map below, a first of its kind feature!</p>
         <div className="relative -mt-6">
           <img src="./sbs-earth/map.png" alt="map" className="w-full" />
           {data.items &&
